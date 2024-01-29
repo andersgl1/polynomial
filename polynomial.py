a3f8560c30b02d6e3a78bc4413778d68a6269256
Anderson Lawrence <andersglawrence@gmail.com>
2024-01-28 19:03:54 -0500

    cs 506 part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,10 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x):
+        return x
+    
 
 class Int:
     def __init__(self, i):
@@ -11,14 +15,20 @@ class Int:
 
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-
+    
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -34,6 +44,9 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
     
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+    
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -41,6 +54,9 @@ class Sub:
 
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
     
 class Div:
     def __init__(self, p1, p2):
@@ -55,7 +71,12 @@ class Div:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " / ( " + repr(self.p2) + " )"
         return repr(self.p1) + " / " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
 
-
-poly = Add( Add( Int(4), Int(3)), Div( X(), Mul( Int(1), Add( Div(X(), X()), Int(1)))))
-print(poly)
+num = 2
+poly = Mul(Div(Add(X(), Int(1)), Int(3)), Int(4))
+#Add( Add( Int(4), Int(3)), Div( X(), Mul( Int(1), Add( Div(X(), X()), Int(1)))))
+print(poly, "| where x =", num)
+print(poly.evaluate(num))
